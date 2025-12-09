b78ff22fa9f82ba98a1dfcb07fdc2986a9cf0a55
MAxLld87 <lalandem87@gmail.com>
2025-12-09 12:03:56 +0100

    Finished first modale and start modal that add picture

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -146,6 +146,9 @@ function launchModale() {
     imgProject.src = works.imageUrl;
     const trashCan = document.createElement("i");
     trashCan.classList.add("fa-solid", "fa-trash-can");
+    trashCan.addEventListener("click", () => {
+      deleteWorks(works.id, imageWrapper);
+    });
     imageWrapper.appendChild(imgProject);
     imageWrapper.appendChild(trashCan);
     imgContainer.appendChild(imageWrapper);
@@ -158,6 +161,9 @@ function launchModale() {
   const formModale = document.createElement("form");
   const btnAddImg = document.createElement("button");
   btnAddImg.textContent = "Ajouter une photo";
+  btnAddImg.addEventListener("click", () => {
+    launchModalAddPicture();
+  });
   divFormModale.appendChild(formModale);
   divFormModale.appendChild(btnAddImg);
   modale.appendChild(divFormModale);
@@ -170,6 +176,86 @@ function launchModale() {
 
   closeIcon.addEventListener("click", () => {
     modale.remove();
-    overlay.classList.remove("overlay");
+    overlay.remove();
   });
+
+  overlay.addEventListener("click", () => {
+    modale.remove();
+    overlay.remove();
+  });
+}
+
+// function to delete any works
+
+async function deleteWorks(workId, ElementToRemove) {
+  const token = localStorage.getItem("token");
+  const request = await fetch(`http://localhost:5678/api/works/${workId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  // if request is ok, we delete work that got same id
+  if (request.ok === true) {
+    ElementToRemove.remove();
+    const workfilter = allWorks.filter((work) => work.id !== workId);
+    loadWorks(workfilter);
+  }
+}
+
+// async function sendNewWorks(){
+
+//   const request = await fetch("http://localhost:5678/api/works",{
+//     method: "POST",
+//     headers: "Content-type: application/json",
+//     image:,
+//     title:,
+//     category:
+//   });
+// }
+
+function launchModalAddPicture() {
+  const categories = fetch("http://localhost:5678/api/categories").then(
+    (response) => response.json()
+  );
+  const modal = document.createElement("div");
+  const previousPage = document.createElement("i");
+  previousPage.classList.add("fa-solid", "fa-arrow-left");
+  modal.appendChild(previousPage);
+  const closeIcon = document.createElement("i");
+  closeIcon.classList.add("fa-solid", "fa-xmark");
+  modal.appendChild(closeIcon);
+  const titleModal = document.createElement("h4");
+  titleModal.innerText = "Ajout photo";
+  modal.appendChild(titleModal);
+  const selectPicture = document.createElement("i");
+  selectPicture.classList.add("fa-regular", "fa-image");
+  modal.appendChild(selectPicture);
+  const buttonAddPicture = document.createElement("button");
+  buttonAddPicture.textContent = "+ Ajouter photo";
+  modal.appendChild(buttonAddPicture);
+  const info = document.createElement("p");
+  info.innerText = "jpg, png : 4mo max";
+  modal.appendChild(info);
+
+  // create form
+
+  const divForm = document.createElement("div");
+  const form = document.createElement("form");
+  divForm.appendChild(form);
+  const labelTitle = document.createElement("label");
+  labelTitle.innerText = "Titre";
+  form.appendChild(labelTitle);
+  const inputTitle = document.createElement("input");
+  labelTitle.appendChild(inputTitle);
+  const selectCategories = document.createElement("select");
+  categories.forEach((element) => {
+    const option = document.createElement("option");
+    option.value = element.id;
+    option.innerText = element.name;
+    selectCategories.appendChild(categories);
+  });
+  form.appendChild(selectCategories);
+  modal.appendChild(form);
+  document.body.appendChild(modal);
 }
